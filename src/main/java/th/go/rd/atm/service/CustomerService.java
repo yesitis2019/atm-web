351dcc66ad5efeb96149e02b467392d6d89e46e5
robert lee <yesitis2019@gmail.com>
2020-08-27 10:52:11 +0700

    form layout day4

--- src/main/java/th/go/rd/atm/service/CustomerService.java
+++ src/main/java/th/go/rd/atm/service/CustomerService.java
@@ -1,25 +1,51 @@
 package th.go.rd.atm.service;
 
+import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.stereotype.Service;
 import th.go.rd.atm.model.Customer;
 
-import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class CustomerService {
-    private ArrayList<Customer> customerList;
-    @PostConstruct
-    public void postConTruct(){
-        customerList = new ArrayList<>();
 
-    }
+    private ArrayList<Customer> customerList = new ArrayList<>();
 
-    public void createCustomer(Customer customer){
+    public void createCustomer(Customer customer) {
+        String hashPin = hash(customer.getPin());
+        customer.setPin(hashPin);
         customerList.add(customer);
     }
-    public List<Customer> getCustomers(){
-        return new ArrayList<>(this.customerList);
+
+    public List<Customer> getCustomers() {
+        return new ArrayList<>(customerList);
+    }
+    public Customer findCustomer(int id) {
+        for (Customer customer : customerList) {
+            if (customer.getId() == id)
+                return customer;
+        }
+        return null;
+    }
+
+    public Customer checkPin(Customer inputCustomer) {
+        // 1. หา customer ที่มี id ตรงกับพารามิเตอร์
+        Customer storedCustomer = findCustomer(inputCustomer.getId());
+
+        // 2. ถ้ามี id ตรง ให้เช็ค pin ว่าตรงกันไหม โดยใช้ฟังก์ชันเกี่ยวกับ hash
+        if (storedCustomer != null) {
+            String storedPin = storedCustomer.getPin();
+            if (BCrypt.checkpw(inputCustomer.getPin(), storedPin))
+                return storedCustomer;
+        }
+        // 3. ถ้าไม่ตรง ต้องคืนค่า null
+        return null;
+    }
+
+    private String hash(String pin) {
+        String salt = BCrypt.gensalt(12);
+        return BCrypt.hashpw(pin, salt);
     }
 }
+
